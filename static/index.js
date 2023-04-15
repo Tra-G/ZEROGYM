52957b430a409c881d8a440cf74717bb415c13a5
cloobTech <belkid98@gmail.com>
2023-04-15 23:05:46 +0100

    90% done with dashboard

---
+++ static/index.js
@@ -0,0 +1,120 @@
+// QUERY SELECTORS
+const sidebarList = document.querySelector('.dashboard-lists');
+const dasbBoardModal = document.querySelector('.dashboard-modal');
+const openSideBar = document.querySelector('.mobile-menu-open');
+const closeSidebar = document.querySelector('.mobile-menu-close');
+
+// Open/Show sidebar
+function showSideBar() {
+    let sidebar = document.querySelector('#sidebar');
+    sidebar.style.left = 0;
+}
+
+// Close sideBar
+function closeSideBar() {
+    let sidebar = document.querySelector('#sidebar');
+    sidebar.style.left = '-100%';
+    
+}
+
+
+// closeNav when ever any part of the modal is clicked
+dasbBoardModal.addEventListener('click', closeSideBar);
+
+// Close Sidebar -- mobile view
+closeSidebar.addEventListener('click', closeSideBar);
+
+// Open side bar -- mobile view
+openSideBar.addEventListener('click', showSideBar);
+
+
+
+// Event Listener to handle side bar
+sidebarList.addEventListener('click', (e) => {
+    const displayHeading = document.querySelector('.display-heading');
+    const welcomeMsg = document.querySelector('.welcome');
+
+    // Hide Welcome Msg
+    welcomeMsg.style.display = 'none';
+    const target = e.target;
+    lists = document.querySelectorAll('.dashboard-list');
+    lists.forEach(list => {
+        // remove active class when a list is clicked
+        if (list.classList.contains('active')) {
+            list.classList.remove('active');
+        }
+        // set active classs 
+        if (list == target) {
+            list.classList.add('active')
+        }
+    })
+    
+    const modalName = target.getAttribute('data-modal');
+    displayHeading.innerHTML = modalName;
+    // Show the modal for the clicked item
+    displayModal(modalName);
+    closeSideBar();
+});
+
+
+// Function to handle modal_display
+function displayModal(modalName) {
+    const modalContainer = document.querySelectorAll('.modal-content');
+    
+    // Set hide modal contents
+    modalContainer.forEach((element) => element.style.display = 'none');
+    const modalContent = document.querySelector(`.${modalName}`);
+    // display the modal
+    modalContent.style.display = 'block';
+}
+
+// Calendar Event (Library -- FullCalendar.io)
+document.addEventListener('DOMContentLoaded', function() {
+var calendarEl = document.getElementById('calendar');
+var calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth',
+        events: [
+{
+start: '2023-04-05T10:00:00',
+end: '2023-04-06T16:00:00',
+title: 'Chest Work out',
+    },
+
+{
+start: '2023-04-11T10:00:00',
+end: '2023-04-12T16:00:00',
+    title: 'Chest Work out',
+    backgroundColor: 'red',
+    color: 'blue',
+},
+
+{
+    start: '2023-04-14T10:00:00',
+    end: '2023-04-15T16:00:00',
+    title: 'Abs',
+},
+
+{
+    start: '2023-04-23T10:00:00',
+    end: '2023-04-24T16:00:00',
+title: 'Thigh',
+},
+    
+{
+start: '2023-04-27T10:00:00',
+end: '2023-04-30T16:00:00',
+    title: 'Muscles',
+display: 'background'
+},
+    
+{
+start: '2023-04-14T10:00:00',
+end: '2023-04-15T16:00:00',
+title: 'Chest Work out',
+}      
+    ],
+
+});
+calendar.render();
+});
+
