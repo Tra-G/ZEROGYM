b3ac32699344ea95734c5473d6ac07abed235015
Adeleke <ogunsonaadedotun@gmail.com>
2023-04-19 17:16:49 +0100

    Added signup page correction

--- views/auth/login.php
+++ views/auth/login.php
@@ -7,7 +7,7 @@
     <title><?php echo $title; ?></title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
     <!-- <link rel="stylesheet" href="path/to/font-awesome/css/all.min.css"> -->
-    <link rel="stylesheet" href="<?php echo assets('css/login.css'); ?>">
+    <link rel="stylesheet" href="<?php echo assets('css/login2.css'); ?>">
     <link rel="stylesheet" href="<?php echo assets('css/global.css'); ?>">
     <!--ICON LINK-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
--- views/auth/register.php
+++ views/auth/register.php
@@ -7,20 +7,15 @@
     <title><?php echo $title; ?></title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
     <!-- <link rel="stylesheet" href="path/to/font-awesome/css/all.min.css"> -->
-    <link rel="stylesheet" href="<?php echo assets('css/signup.css'); ?>">
+    <link rel="stylesheet" href="<?php echo assets('css/signup2.css'); ?>">
     <link rel="stylesheet" href="<?php echo assets('css/global.css'); ?>">
     <!--ICON LINK-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
     <div class="container-all">
-        <div class="left-column">
+        <div class="column" id="left-column">
 
-
-            <div class="form-content">
-                <form style="width:inherit" action="" method="post">
-                    <div class="form-container">
-                    <div>
                         <div class="titlehead">
                             <div class="nav-title">
                                 <a href="<?php echo redirect(''); ?>">
@@ -43,38 +38,40 @@
                                     <?php endif; ?>
                             </div>
                         </div>
+                        <form action="" method="post">
 
                             <div class="signup">
 
                                 <div>
                                     <div class="textinput">
                                         <i style="color: #DEEF0B;" class="fa fa-user" aria-hidden="true"></i>
-                                        <input name="name" type="text" placeholder="Full Name">
+                                        <input name="name" type="text" placeholder="Enter your full name">
                                     </div>
 
                                     <div class="textinput">
                                         <i style="color: #DEEF0B;" class="fa fa-envelope" aria-hidden="true"></i>
-                                        <input name="email" type="email" placeholder="Email">
+                                        <input name="email" type="email" placeholder="Enter your Email">
                                     </div>
 
-                                    <div class="textinput">
-                                        <i style="color: #DEEF0B;" class="fa fa-phone" aria-hidden="true"></i>
-                                        <input name="phone" type="tel" placeholder="Phone Number">
+                        <div class="textinput" style="margin-left: 1rem; ">
+                            <i style="color: #DEEF0B; position: absolute;" class="fa-sharp fa-solid fa-phone" aria-hidden="true"></i>
+                            <input name="phone" type="phone" placeholder="Enter your Phone Number">
                                     </div>
 
-                                    <div class="textinput">
-                                        <i style="color: #DEEF0B;" class="fa fa-map-marker-alt" aria-hidden="true"></i>
-                                        <input name="address" type="text" placeholder="Address">
+                        <div class="textinput" style="margin-left: 1rem; ">
+                            <i style="color: #DEEF0B; position: absolute;" class="fa-solid fa-house" aria-hidden="true"></i>
+                            <input name="address" type="text" placeholder="Enter your Address">
                                     </div>
 
-                                    <div class="textinput">
-                                        <i style="color: #DEEF0B;" class="fa fa-city" aria-hidden="true"></i>
-                                        <input name="city" type="text" placeholder="City">
+
+                        <div class="textinput" style="margin-left: 1rem; ">
+                            <i style="color: #DEEF0B; position: absolute;" class="fa-solid fa-city" aria-hidden="true"></i>
+                            <input name="city" type="text" placeholder="Enter your City">
                                     </div>
 
-                                    <div class="textinput">
-                                        <i style="color: #DEEF0B;" class="fa fa-map" aria-hidden="true"></i>
-                                        <input name="state" type="text" placeholder="State">
+                        <div class="textinput" style="margin-left: 1rem; ">
+                            <i style="color: #DEEF0B;position: absolute;" class="fa-solid fa-location-dot" aria-hidden="true"></i>
+                            <input name="state" type="text" placeholder="Enter your State">
                                     </div>
 
                                     <div class="textinput">
@@ -84,7 +81,7 @@
 
                                     <div class="reminder">
                                             <input type="checkbox">
-                                            <span>I agree to the Terms and Conditions</span>
+                                <span>I read and agree to Terms and Conditions</span>
                                         </div>
 
                                     <div>
@@ -94,28 +91,22 @@
                                     </div>
 
                                     <div class="log">
-                                        Already have an account? <a style="color: #fff;" href="<?php echo redirect('login'); ?>">Login</a>
+                            Already have an account? <a style="color: #DEEF0B;" href="<?php echo redirect('login'); ?>">Login</a>
                                     </div>
                                 </div>
                             </div>
                             <div class="bottom"></div>
-                        </div>
-                    </div>
+
+
 
                         </form>
-
-                    </div>
-
                 </div>
-                <div class="right-column">
-            <!-- Right column content -->
-            <div>
-
-
+        <div class="column"  id="right-column">
+            <div style="height:100%" >
+                <img src="<?php echo assets('images/img9.jpg') ?>" alt="Image" />
             </div>
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-element-bundle.min.js"></script>
 </body>
 </html>
