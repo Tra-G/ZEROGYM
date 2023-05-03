0824d9dc99eadabc8e0bf463326a05de63c3d55e
Adeleke <ogunsonaadedotun@gmail.com>
2023-05-03 20:40:20 +0100

    Added new phpmailer

--- models/functions.php
+++ models/functions.php
@@ -1,9 +1,9 @@
 <?php
 
 // Load PHPMailer classes
-require_once(__DIR__.'/PHPMailer/src/PHPMailer.php');
-require_once(__DIR__.'/PHPMailer/src/SMTP.php');
-require_once(__DIR__.'/PHPMailer/src/Exception.php');
+require_once(__DIR__.'/phpmailer/src/PHPMailer.php');
+require_once(__DIR__.'/phpmailer/src/SMTP.php');
+require_once(__DIR__.'/phpmailer/src/Exception.php');
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
