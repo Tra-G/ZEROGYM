63bf8537f9c69a2ed9cf1cf949eb5a8bfddaf213
Adeleke <ogunsonaadedotun@gmail.com>
2023-05-04 16:34:08 +0100

    Modified login page

--- views/auth/login.php
+++ views/auth/login.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="<?php echo assets('css/login.css'); ?>">
     <link rel="stylesheet" href="<?php echo assets('css/global.css'); ?>">
     <!--ICON LINK-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 
 <body>
