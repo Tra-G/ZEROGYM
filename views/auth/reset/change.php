69cf36eb5176522df2da3ce37162f0d986cabaa5
Adeleke <ogunsonaadedotun@gmail.com>
2023-05-05 14:27:25 +0100

    Changed password reset page

--- views/auth/reset/change.php
+++ views/auth/reset/change.php
@@ -13,21 +13,19 @@
 
 <body>
     <h2>Password Reset</h2>
-    <form id="myForm" onsubmit="return submitForm()" method="post">
-        <div class="container">
-            <div class="card">
-                <!-- <h1>Password Reset</h1> -->
-                <form>
-                    <p id="red-text" class="red-text"></P>
-                    <label style="margin-top: 1rem" for="new-password">New Password:</label>
-                    <input name="password" type="password" id="new-password" name="new-password" required>
-                    <label for="confirm_password">Confirm Password:</label>
-                    <input name="confirm_password" type="password" id="confirm_password" name="confirm-password"
-                        required>
-                    <button type="submit">Submit</button>
-                </form>
-            </div>
+    <div class="container">
+        <div class="card">
+            <!-- <h1>Password Reset</h1> -->
+            <form id="myForm" onsubmit="return submitForm()" method="post">
+                <p id="red-text" class="red-text"></P>
+                <label style="margin-top: 1rem color:#5c5656" for="new-password">New Password:</label>
+                <input name="password" type="password" id="new-password" required>
+                <label style="color:#5c5656" for="confirm-password">Confirm Password:</label>
+                <input name="confirm_password" type="password" id="confirm-password" required>
+                <button type="submit">Submit</button>
+            </form>
         </div>
+    </div>
     </form>
 </body>
 
@@ -39,9 +37,11 @@
         xhr.onreadystatechange = function () {
             if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
                 document.getElementById("red-text").innerHTML = xhr.responseText;
-                setTimeout(function () {
-                    window.location.href = "<?php echo redirect('login'); ?>";
-                }, 2000); // Redirect after 2 seconds.
+                if (xhr.responseText == "Password changed successfully. You can now login.") {
+                    setTimeout(function () {
+                        window.location.href = "<?php echo redirect('login'); ?>";
+                    }, 2000); // Redirect after 2 seconds.
+                }
             }
         };
         xhr.open("POST", "<?php echo $token; ?>/change", true);
